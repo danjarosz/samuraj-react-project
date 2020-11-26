ee71c44697d584c368f6541a0e7eaf877cf5a5d6
danjarosz <jarosz.daniel@protonmail.com>
2020-11-26 22:32:57 +0100

    feat: implement app store and request helper

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import StoreProvider from "./store/StoreProvider";
 import './App.scss';
 
 const App = () => (
-  <header>
-    Hello React!
-  </header>
+  <StoreProvider>
+    <header>
+      Hello React!
+    </header>
+  </StoreProvider>
 );
 
 export default App;
